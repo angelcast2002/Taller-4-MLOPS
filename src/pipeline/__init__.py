ac901b1f3f50b411b8e61bf3a6ef2a7c8567d9ca
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2025-09-09 19:02:21 -0600

    final
    Co-Authored-By: sara <sarap.echeverria@gmail.com>
    Co-Authored-By: Diego Alejandro Morales Escobaar <77738746+diego2250@users.noreply.github.com>
    Co-Authored-By: Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,12 +1,1 @@
-# src/pipeline_ml/__init__.py
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("pipeline")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
-
-# Exporta utilidades del paquete
-from .pipeline import cli as train_cli  # permite: `from pipeline_ml import train_cli`
-
-__all__ = ["__version__", "train_cli"]
+__all__ = []
